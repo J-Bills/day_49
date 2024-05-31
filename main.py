af943fd44349cd66d4ba3ea9ecbcff4a56f3e2f0
J-Bills <billsjamal@gmail.com>
2024-05-31 16:36:56 -0700

    finishing automating job search query

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from config import credential_dict, query_dict
+import config
 
 def main():
     chrome_options = webdriver.ChromeOptions()
@@ -15,8 +15,8 @@ def login(driver):
     driver.get('https://www.linkedin.com/login')
     username_field = driver.find_element(By.ID,value='username')
     password_field = driver.find_element(By.ID,value='password')
-    username_field.send_keys(credential_dict.get('username'))
-    password_field.send_keys(credential_dict.get('password'))
+    username_field.send_keys(config.credential_dict.get('username'))
+    password_field.send_keys(config.credential_dict.get('password'))
     
     sign_in_button = driver.find_element(By.CLASS_NAME, value="btn__primary--large")
     print(sign_in_button.text)
@@ -25,18 +25,32 @@ def login(driver):
 def jobSearch(driver):
     driver.get('https://www.linkedin.com/jobs')
     
-    if len(query_dict) == 0:
-        job_title_input = input("Enter the job title that you want")
-        location_input = input("Enter the location")
-        query_dict['job_title'] = job_title_input
-        query_dict['location'] = location_input
+    if len(config.query_dict) == 0:
+        job_title_input = input("Enter the job title that you want:\n")
+        location_input = input("Enter the location:\n")
+        config.query_dict['job_title'] = job_title_input
+        config.query_dict['location'] = location_input
 
-    driver.implicity_wait(5)
-    job_text = driver.find_element(By.CLASS_NAME, value="jobs-search-box__text-input")
-    job_text.send_keys(query_dict['job_title'])
+    driver.implicitly_wait(5)
     
+    ember = driver.find_element(By.CLASS_NAME, value="ember-view")
+    ember_value = ember.get_attribute('id')
+    
+    job_text = driver.find_element(By.CLASS_NAME, value="jobs-search-box__text-input")
+    job_text.send_keys(config.query_dict['job_title'])
+    job_text.click()
+        
+    location_text = driver.find_element(By.ID, value=f"jobs-search-box-location-id-{ember_value}")
+    location_text.send_keys(config.query_dict['location'])
+    location_text.send_keys(Keys.ENTER)
+
+def jobApply(driver):
+    # easy_apply_button = driver.find_element(By.XPATH, value='//*[@id="ember380"]')
+    pass
+
 
 if __name__ == "__main__":
     browser = main()
     login(browser)
     jobSearch(browser)
+    jobApply(browser)
